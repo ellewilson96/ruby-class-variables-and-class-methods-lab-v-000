abfe1cd6151964fa50d9986ced0dcb22464953bd
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-27 17:55:18 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,25 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre)
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq!
+  end
+
+  def self.genres
+    @@genres.uniq!
+  end
+end
