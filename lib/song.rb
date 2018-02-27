fc0c1b1dfa546e12f15449e4bb7d3e5584f8a5de
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-27 17:58:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
   @@genres = []
   @@artists = []
 
-  def initialize(@name, @artist, @genre)
+  def initialize(name, artist, genre)
     @@count += 1
     @@genres << genre
     @@artists << artist
